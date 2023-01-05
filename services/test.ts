8db5747ef4d340d1ad16e8839010406acaf1e67f
Seanghor <hai.seanghor009@gmail.com>
2023-01-05 23:57:08 +0700

    styling code for my project

--- services/test.ts
+++ services/test.ts
@@ -1,6 +1,6 @@
 
 // import { prisma } from '../prisma/db';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 const findCustomerById = async (id:number) => {
